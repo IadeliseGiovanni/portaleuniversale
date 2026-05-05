c3c8aaf198dda65c5d5567ffcbb5a5554afe973c
Giovanni Iadelise <iadelisegiovanni2000@gmail.com>
2026-05-05 09:26:03 +0200

    Add files via upload

---
+++ portaleuniversale/portaleuniversale/src/main/java/com/scuola/portaleuniversale/PortaleuniversaleApplication.java
@@ -0,0 +1,13 @@
+package com.scuola.portaleuniversale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortaleuniversaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortaleuniversaleApplication.class, args);
+	}
+
+}
---
+++ portaleuniversale/portaleuniversale/src/test/java/com/scuola/portaleuniversale/PortaleuniversaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scuola.portaleuniversale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortaleuniversaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
